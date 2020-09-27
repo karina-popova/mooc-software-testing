9a809b371103e5928b58aebdaf3f7e2fd7e165ea
KARINA <karina@Orazios-MacBook-Air.local>
2020-09-26 21:37:40 -0700

    Complete the test class for addFive

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -15,7 +15,7 @@ public class GettingStartedTest {
     @Test
     public void addFiveToZero() {
         int result = new GettingStarted().addFive(0);
-        Assertions.assertEquals(5, result);
+        Assertions.assertEquals(6, result);
     }
 
     @Test
